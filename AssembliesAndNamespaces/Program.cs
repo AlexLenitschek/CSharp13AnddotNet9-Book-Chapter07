ba929c6c1ab55630d4bd0f3bc2d1b1c33d0e37d5
alex lenitschek <Alexander.lenitschek@gmx.de>
2025-02-09 13:54:05 +0100

    Add project files.

---
+++ AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Linq; // To use XDocument
+using System; // To use String.
+
+XDocument doc = new();
+
+#region Relating C# keywords to .NET types
+// Capital Letters symbolize the .NET types from System.
+// Use the C# types which are denoted by non-capital letters.
+string s1 = "Hello";
+String s2 = "World!";
+WriteLine($"{s1} {s2}");
+WriteLine();
+#endregion
+
