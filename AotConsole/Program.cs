afe9600a243afb08cfb42e5ec2135dc38637cefe
alex lenitschek <Alexander.lenitschek@gmx.de>
2025-02-11 16:38:38 +0100

    Adding additional Chapter07 stuff. Publishing and Uploading to nuget.org, JIT and AOT

---
+++ AotConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization; // To use CultureInfo.
+using System.Reflection; // To use AssemblyName.
+using System.Reflection.Emit; // To use AssemblyBuilder.
+
+WriteLine("This is an Ahead-Of-Time (AOT) compiled console app.");
+WriteLine("Current culture: {0}", CultureInfo.CurrentCulture.DisplayName);
+WriteLine("OS version: {0}", Environment.OSVersion);
+
+Write("Press any key to exit.");
+ReadKey(intercept: true); // Do not output the key that was pressed.
+
+#region Publishing a native AOT project
+// The following has the Tag [RequiresDynamicCodeAttribute] which can break when AOT compiling.
+//AssemblyBuilder ab = AssemblyBuilder.DefineDynamicAssembly
+//  (new AssemblyName("My Assembly"), AssemblyBuilderAccess.Run);
+#endregion
--- AssembliesAndNamespaces/Program.cs
+++ AssembliesAndNamespaces/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Xml.Linq; // To use XDocument
 using System; // To use String.
+using Packt.Shared;
+
 
 XDocument doc = new();
 
@@ -12,3 +14,21 @@ WriteLine($"{s1} {s2}");
 WriteLine();
 #endregion
 
+#region Testing your class library package
+
+Write("Enter a color value in hex: ");
+string? hex = ReadLine();
+WriteLine("Is {0} a valid color value? {1}",
+  arg0: hex, arg1: hex.IsValidHex());
+
+Write("Enter a XML element: ");
+string? xmlTag = ReadLine();
+WriteLine("Is {0} a valid XML element? {1}",
+  arg0: xmlTag, arg1: xmlTag.IsValidXmlTag());
+
+Write("Enter a password: ");
+string? password = ReadLine();
+WriteLine("Is {0} a valid password? {1}",
+  arg0: password, arg1: password.IsValidPasswort());
+
+#endregion
